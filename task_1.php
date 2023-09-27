288b182efca59dbce0fdd7b8ef515518a9f8d502
Probir-Ray <somironroy.need@gmail.com>
2023-09-27 22:50:43 +0600

    Task-1

--- task_1.php
+++ task_1.php
@@ -2,7 +2,21 @@
 
 /**** 
  * Task 1: String Manipulation
- * 
+ * Create a string variable called $text with the value "The quick brown fox jumps over the lazy dog.". Write a PHP function which takes "$text" as an argument to:
+ * Convert the string to all lowercase.
+ * Replace "brown" with "red" in the string.
+ * Print the modified text.
  ****/
 
 
+$text = "The quick brown fox jumps over the lazy dog.";
+function text_method($text) {
+    // Convert to all lowercase
+    $convert_to_lowercase = strtolower($text);
+    echo $convert_to_lowercase . PHP_EOL;
+
+    // replace "brown" with "red"
+    $modified_text = str_replace("brown", "red", $text);
+    echo $modified_text;
+}
+text_method($text);
